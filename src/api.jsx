6cfeb1d889876ead249f0cc66d7d186d29f004e1
Usama Ikram <qasim.buttar143@gmail.com>
2023-05-07 14:37:13 -0400

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -3,8 +3,8 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '71b4870b95msh563e0bb290ef232p16e601jsn99261177399a',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+		'X-RapidAPI-Key': 'Your key will go here',
+		'X-RapidAPI-Host': 'Your host will go here'
 	}
 };
 
@@ -12,4 +12,4 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 
-export const WEATHER_API_KEY = '5c48a5471c56dd66ece2f999d5953c87';
+export const WEATHER_API_KEY = 'Your key';
